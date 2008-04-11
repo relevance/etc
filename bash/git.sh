feadfa8b6d7d71039d27fd1eeed2b11fcf9a979f
Rob Sanheim <rsanheim@gmail.com>
2008-04-11 10:11:04 -0400

    git aliases

---
+++ bash/git.sh
@@ -0,0 +1,13 @@
+# Thanks to Geoffrey's peepcode for many of these
+alias g='git'
+alias gst='git status'
+alias gl='git pull'
+alias gp='git push'
+alias gd='git diff | mate'
+alias gc='git commit -v'
+alias gca='git commit -v -a'
+alias gb='git branch'
+alias gba='git branch -a'
+
+alias gsd='git svn dcommit'
+alias gsb='git svn rebase'
