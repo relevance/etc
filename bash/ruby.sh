a2e4366bd460a4fd96be33bf39208413bcfeccdd
Jason Rudolph <jason@thinkrelevance.com>
2008-06-26 19:24:48 -0400

    Add bash shortcut to access rdoc for local rubygems from the command line ...
      - See http://stephencelis.com/archive/2008/6/bashfully-yours-gem-shortcuts
      - Example: run `gemdoc mocha` from the command line to open the
        mocha rdoc in your browser

--- bash/ruby.sh
+++ bash/ruby.sh
@@ -5,9 +5,19 @@ export RUBYOPT=rubygems
 
 complete -C ~/src/scripts/bin/rake_autocompletion.rb -o default rake
 
-# ruby shortcuts
+# rubygems shortcuts (http://stephencelis.com/archive/2008/6/bashfully-yours-gem-shortcuts)
 alias gems='cd /opt/local/lib/ruby/gems/1.8/gems'
+export GEMDIR=`gem env gemdir`
+gemdoc() {
+  open $GEMDIR/doc/`$(which ls) $GEMDIR/doc | grep $1 | sort | tail -1`/rdoc/index.html
+}
+_gemdocomplete() {
+  COMPREPLY=($(compgen -W '$(`which ls` $GEMDIR/doc)' -- ${COMP_WORDS[COMP_CWORD]}))
+  return 0
+}
+complete -o default -o nospace -F _gemdocomplete gemdoc
 
+# ruby shortcuts
 alias sc='script/console'
 alias ss='script/server'
 alias sg='script/generate'
