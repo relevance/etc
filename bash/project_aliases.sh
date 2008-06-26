e51db76e9df6403a2cf6042d99e931a0421047cc
Muness Alrubaie <muness@thinkrelevance.com>
2008-06-25 21:13:05 -0400

    refactoring to better name things make it a little smarter by making sure things are actually directories

--- bash/project_aliases.sh
+++ bash/project_aliases.sh
@@ -1,11 +1,13 @@
-if [ -z "${PROJECT_DIRS[*]}" ]; then
-	echo "Define a PROJECT_DIR array in your ~/.bash_profile"
+if [ -z "${PROJECT_PARENT_DIRS[*]}" ]; then
+	echo "Define a PROJECT_PARENT_DIRS array in ~/.bash_profile"
 fi
 
-for PROJECT_DIR in ${PROJECT_DIRS[@]} ; do
-  if [ -d "$PROJECT_DIR" ]; then
-    for PROJECT in $(ls $PROJECT_DIR); do
-	    alias $PROJECT="cd $PROJECT_DIR/$PROJECT"
+for PARENT_DIR in ${PROJECT_PARENT_DIRS[@]} ; do
+  if [ -d "$PARENT_DIR" ]; then
+    for PROJECT_DIR in $(ls $PARENT_DIR); do
+			if [ -d "$PARENT_DIR/$PROJECT_DIR" ]; then
+	      alias "$PROJECT_DIR"="cd $PARENT_DIR/$PROJECT_DIR"
+			fi
     done
   fi
 done
