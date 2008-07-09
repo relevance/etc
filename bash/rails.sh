d60d239985d1f3839a84e9457829cb15c174c7f0
Rob Sanheim <rsanheim@gmail.com>
2008-07-09 10:36:38 -0400

    split out rails sh

---
+++ bash/rails.sh
@@ -0,0 +1,26 @@
+echo "[ executing ruby.sh ]"
+
+# ruby shortcuts
+alias sc='script/console'
+alias scp='script/console production'
+alias ss='script/server'
+alias sg='script/generate'
+
+# testing shortcuts
+alias rt='rake --trace'
+alias rtf='rake test:functionals --trace'
+alias rti='rake test:integration --trace'
+alias rtl='rake test:lib --trace'
+alias rtp='rake test:plugins --trace'
+alias rtu='rake test:units --trace'
+
+alias rrcov='rake coverage:all:test'
+alias rrrcovall='rake test:coverage:all:test'
+
+alias rdm='rake db:migrate'
+alias rdtp='rake db:test:prepare'
+alias rdfl='rake db:fixtures:load'
+
+# capistrano
+alias csd='cap staging deploy'
+
