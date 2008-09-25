bf53466e7ac5c090c6786008a19a9d5b8cc31940
Rob Sanheim <rsanheim@gmail.com>
2008-09-24 21:07:21 -0400

    load the awesome git autocompletion if its there...

--- bash/git.sh
+++ bash/git.sh
@@ -14,3 +14,9 @@ alias gpp='git pull; git push'
 # For when you are stuck between worlds...
 alias gsd='git svn dcommit'
 alias gsr='git svn rebase' 
+
+# git auto completion - don't write your own when its already been done (better) and is in git-core
+#   sudo port install git-core +bash_completion 
+if [ -f /opt/local/etc/bash_completion ]; then
+	. /opt/local/etc/bash_completion
+fi
