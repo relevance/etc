1a42d6670fc06a0ddaa39651b347a748d60fb062
Rob Sanheim <rsanheim@gmail.com>
2008-11-21 19:22:15 -0500

    add amazon web services bash script

---
+++ bash/aws.sh
@@ -0,0 +1,5 @@
+# Setup for Amazon Web Services
+export EC2_HOME=$HOME/.ec2
+export PATH=$PATH:$EC2_HOME/bin
+# AWS command line tools require Java
+export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home/
