1bc9cb757d6637dc8a799525cd2a5bd9b9ff19fe
Muness Alrubaie <muness@thinkrelevance.com>
2009-04-29 17:02:44 -0400

    support jruby 1.2.0

--- bash/ruby_switcher.sh
+++ bash/ruby_switcher.sh
@@ -11,9 +11,25 @@ function use_jruby_116 {
  export GEM_HOME=~/.gem/jruby/1.8
  update_path
 }
- 
+
+function use_jruby_120 {
+ export MY_RUBY_HOME=~/.ruby_versions/jruby-1.2.0
+ export GEM_HOME=~/.gem/jruby/1.8
+ update_path
+}
+
+function install_jruby_120 {
+  mkdir -p ~/.ruby_versions && pushd ~/.ruby_versions &&
+  curl -O -L --silent http://dist.codehaus.org/jruby/1.2.0/jruby-bin-1.2.0.zip &&
+  rm -rf jruby-1.2.0 &&
+  jar xf jruby-bin-1.2.0.zip &&
+  ln -sf ~/.ruby_versions/jruby-1.2.0/bin/jruby ~/.ruby_versions/jruby-1.2.0/bin/ruby &&
+  chmod +x ~/.ruby_versions/jruby-1.2.0/bin/jruby &&
+  popd
+}
+
 function use_ree_186 {
- export MY_RUBY_HOME=/opt/ruby-enterprise-1.8.6-20090201
+ export MY_RUBY_HOME=/opt/ruby-enterprise-1.8.6-20090421
  export GEM_HOME=~/.gem/ruby/1.8
  update_path
 }
@@ -32,7 +48,7 @@ function update_path {
 
 function display_ruby_version {
  if [[ $SHELL =~ "bash" ]]; then
-   echo "Using $RUBY_VERSION"
+   echo "Using `ruby -v`"
  fi
  # On ZSH, show it on the right PS1
  export RPS1=$RUBY_VERSION
