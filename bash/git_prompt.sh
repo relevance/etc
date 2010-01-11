3967a13c4fcf385bdb40f49f74643782c0871565
Rob Sanheim <rsanheim@gmail.com>
2010-01-11 09:39:48 -0500

    Be explicit and show if you are solo in your prompt

--- bash/git_prompt.sh
+++ bash/git_prompt.sh
@@ -44,7 +44,7 @@ function git_branch_and_user {
 # 
 #   git config --global user.initials 'jr'
 function git_user_initials {
-  git config --get user.initials
+  git config --get user.initials || echo "-solo-"
 }
 
 function prompt_func() {
