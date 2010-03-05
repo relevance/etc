1c9c522282688f07828d10fffcd385f5c32d0767
Rob Sanheim <rsanheim@gmail.com>
2010-03-05 10:13:05 -0500

    remove gem doc stuff, seems flaky

--- bash/ruby.sh
+++ bash/ruby.sh
@@ -2,15 +2,6 @@ alias relock='bundle install --relock'
 
 # rubygems shortcuts (http://stephencelis.com/archive/2008/6/bashfully-yours-gem-shortcuts)
 alias gems='cd $(gem env gemdir)/gems'
-export GEMDIR=`gem env gemdir`
-gemdoc() {
-  open $GEMDIR/doc/`$(which ls) $GEMDIR/doc | grep $1 | sort | tail -1`/rdoc/index.html
-}
-_gemdocomplete() {
-  COMPREPLY=($(compgen -W '$(`which ls` $GEMDIR/doc)' -- ${COMP_WORDS[COMP_CWORD]}))
-  return 0
-}
-complete -o default -o nospace -F _gemdocomplete gemdoc
 
 gemlite() {
   gem install $1 --no-rdoc --no-ri
