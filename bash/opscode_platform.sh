e1d16e1db0270527d33513bc4b137f3810da24f5
Michael Nygard <michael.nygard@n6consulting.com>
2011-06-22 15:41:13 -0500

    Add bash script for Opscode org. Useful for cucumber-chef.

---
+++ bash/opscode_platform.sh
@@ -0,0 +1,14 @@
+# See http://github.com/relevance/company-wiki/wiki/Chef-Platform for usage and explanation
+
+# Add lines like these to your own .bashrc like this:
+
+# This is the personal account you created
+#export OPSCODE_USER=mtnygard
+
+# These are available from the usual place. I recommend catting them in:
+#export AWS_ACCESS_KEY_ID=`cat /Volumes/THEREALDEAL/relevance_aws/access_key.txt`
+#export AWS_SECRET_ACCESS_KEY=`cat /Volumes/THEREALDEAL/relevance_aws/secret_access_key.txt`
+#export AWS_SSH_KEY_ID=/Volumes/THEREALDEAL/relevance_aws.pem
+
+export ORGNAME=relevance
+
