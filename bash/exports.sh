69fa59662214330c05b34c948b051bfb102e67c9
Rob Sanheim <rsanheim@gmail.com>
2011-10-24 14:26:59 -0500

    updates to not assume Snow Leopard or Lion:
      - remove Java Home ...not sure what this would be on Lion
      - remove arch flag

--- bash/exports.sh
+++ bash/exports.sh
@@ -1,16 +1,9 @@
-# Set default console Java to 1.6
-export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
-
 export CLICOLOR=1
 export LSCOLORS=gxfxcxdxbxegedabagacad
 
 # Enable color in grep
 export GREP_OPTIONS='--color=auto' 
 export GREP_COLOR='3;33'
-
-# This resolves issues install the mysql, postgres, and other gems with native non universal binary extensions
-# You only want this if you are on Snow Leopard
-export ARCHFLAGS='-arch x86_64'
 
 # History: don't store duplicates
 export HISTCONTROL=erasedups  
