3efacb3e1d765bde97e554fa39ec36de7b5c8b8b
Relevance Pair <pair@thinkrelevance.com>
2012-04-30 15:42:30 -0400

    Add ssh-reagent bash function

---
+++ bash/reagent.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function ssh-reagent {
+    for agent in /tmp/ssh-*/agent.*; do
+        export SSH_AUTH_SOCK=$agent
+        if ssh-add -l 2>&1 > /dev/null; then
+            echo Found working SSH Agent:
+            ssh-add -l
+            return
+        fi
+    done
+    echo Cannot find ssh agent - maybe you should reconnect and forward it?
+}
